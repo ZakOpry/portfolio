a75340ac5a64a82fb6db2170d961f23b4c5c25c0
ZakOpry <88099668+ZakOpry@users.noreply.github.com>
2022-01-24 10:10:21 -0500

    Added Function to close menu after clicking link

--- src/components/HamburgerNavBar.js
+++ src/components/HamburgerNavBar.js
@@ -1,32 +1,51 @@
-import React from 'react'
-import '../styles/HamburgerNavBarStyles.css'
-import { useState } from 'react'
-import {AiOutlineMenu} from 'react-icons/ai'
-import { IconContext } from 'react-icons'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "../styles/HamburgerNavBarStyles.css";
+import { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { IconContext } from "react-icons";
+import { Link } from "react-router-dom";
 export default function HamburgerNavBar() {
-    const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
 
-
-    return (
-        <>
-        <div className = "mainMobileNavBarDiv">
-            <div className="menuDiv" onClick={()=> setMenuOpen( ! menuOpen)}>
-                <IconContext.Provider value= {{className: "menuIcon"}}>
-                <AiOutlineMenu/>
-                 </IconContext.Provider>
-                </div> 
+  return (
+    <>
+      <div className="mainMobileNavBarDiv">
+        <div className="menuDiv" onClick={() => setMenuOpen(!menuOpen)}>
+          <IconContext.Provider value={{ className: "menuIcon" }}>
+            <AiOutlineMenu />
+          </IconContext.Provider>
         </div>
-            
-        {menuOpen ? <div className="dropDown">
-             <Link className="link" to="/"><div className="navBarButton" id="home"><h3>About Me</h3></div></Link>
-             <Link className="link" to="/projects"> <div className="navBarButton" id="dropDownProjects"><h3>Projects</h3></div></Link>
-             <Link className="link" to="/contact"><div className="navBarButton" id="dropDownContact"><h3>Contact</h3></div></Link>
+      </div>
 
-             </div> : <></>}
-
-
-
-        </>
-    )
+      {menuOpen ? (
+        <div className="dropDown">
+          <Link className="link" to="/" onClick={() => setMenuOpen(!menuOpen)}>
+            <div className="navBarButton" id="home">
+              <h3>About Me</h3>
+            </div>
+          </Link>
+          <Link
+            className="link"
+            to="/projects"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <div className="navBarButton" id="dropDownProjects">
+              <h3>Projects</h3>
+            </div>
+          </Link>
+          <Link
+            className="link"
+            to="/contact"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <div className="navBarButton" id="dropDownContact">
+              <h3>Contact</h3>
+            </div>
+          </Link>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 }
